01dbd1f2688a3374391786fa478be6f119d800be
Darren Kulp <darren@kulp.ch>
2020-07-15 03:58:02 -0700

    fixup! Enable disassembler during configuration

--- build-lightning.sh
+++ build-lightning.sh
@@ -5,7 +5,7 @@ PREFIX=$1
 RELEASE=$2
 (
     cd $PREFIX/$RELEASE
-    ./configure --prefix=$PREFIX --enable-disassembler
+    ./configure --prefix=$PREFIX ${CARGO_FEATURE_DISASSEMBLY+--enable-disassembler}
     make -j4
     make install
 )
